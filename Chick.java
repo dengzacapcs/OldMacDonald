d4ad535cc6ef250092ee83a379fcfd501b7cf2b8
dengzacapcs <zadeng@s.sfusd.edu>
2017-10-17 14:11:48 -0700

    Add classes

--- Chick.java
+++ Chick.java
@@ -1,4 +1,23 @@
-class Chick //implements Animal 
+class Chick implements Animal 
 {     
-     //your code here
+	private String myType;
+	private String mySound;
+	private String myOtherSound;
+	public Chick(String type, String sound){
+		myType = type;
+		mySound = sound;
+		myOtherSound = "unknown";
+	}
+	public Chick(String type, String firstSound, String secondSound){
+		myType = type;
+		mySound = firstSound;
+		myOtherSound = secondSound;
+	}
+	public Chick(){
+		myType = "unknown";
+		mySound = "unknown";
+		myOtherSound = "unknown";
+	}
+	public String getSound(){return (Math.random() < 0.5 ? mySound : myOtherSound);}
+	public String getType(){return myType;};
 }
--- OldMacDonald.pde
+++ OldMacDonald.pde
@@ -1,6 +1,7 @@
 
 void setup()
 {
-	//your variable declarations here
+	Farm myFarm = new Farm();
+	myFarm.animalSounds();
 }
 
